5ca5d76d9306d59dfaa4738f3cbc97fe267be8ec
LIZA <tearechard12@gmail.com>
2026-01-28 18:31:49 +0300

    Создание проекта и настройка pom.xml

---
+++ src/test/java/com/example/English_Learning_Platform/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.English_Learning_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
